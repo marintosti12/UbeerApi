a56384f7e80da2d8ae149682364791bfa4411e6e
marintosti12 <marin.tostivint@epsi.fr>
2022-06-02 20:19:31 +0200

    remove json encryption

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const cors = require('cors');
 
 app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 const db = require("./models");
 
 db.sequelize.sync({ force: true }).then(() => {
