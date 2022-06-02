830743ec27374075559480184e95a67e754c78cf
marintosti12 <marin.tostivint@epsi.fr>
2022-06-02 19:44:07 +0200

    add crud functions for breweries

--- controllers/brewery.controller.js
+++ controllers/brewery.controller.js
@@ -3,9 +3,25 @@ const Brewery = db.brewerys;
 const Beer = db.beers;
 
 const Op = db.Sequelize.Op;
-// Create and Save a new Tutorial
-exports.create = (req, res) => {
 
+exports.create = (req, res) => {
+    const brewery = {
+        name: req.body.name,
+        city: req.body.city,
+        address: req.body.address,
+        image: req.body.image,
+    };
+
+    Brewery.create(brewery)
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || "Some error occurred while creating the Brewery."
+        });
+    });
 };
 
 // Retrieve all beers from the database.
@@ -43,7 +59,7 @@ exports.findOne = (req, res) => {
 
 // Retrieve all beers from the database.
 exports.paginate = (req, res) => {
-    Brewery.findAll({ include: Beer , limit: req.params.limit, offset: req.params.offset})
+    Brewery.findAndCountAll({ include: Beer , limit: req.params.limit, offset: req.params.offset})
         .then(data => {
             res.send(data);
         })
@@ -57,17 +73,47 @@ exports.paginate = (req, res) => {
 
 // Update a Tutorial by the id in the request
 exports.update = (req, res) => {
-
+    const id = req.params.id;
+    Brewery.update(req.body, {
+        where: { id: id }
+    })
+    .then(num => {
+        if (num == 1) {
+            res.send({
+                message: "Brewery was updated successfully."
+            });
+        } else {
+            res.send({
+                message: `Cannot update Brewery with id=${id}. Maybe Brewery was not found or req.body is empty!`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Error updating Brewery with id=" + id
+        });
+    });
 };
-// Delete a Tutorial with the specified id in the request
+// Delete a Brewery with the specified id in the request
 exports.delete = (req, res) => {
-
-};
-// Delete all Tutorials from the database.
-exports.deleteAll = (req, res) => {
-
-};
-// Find all published Tutorials
-exports.findAllPublished = (req, res) => {
-
+    const id = req.params.id;
+    Brewery.destroy({
+        where: { id: id }
+    })
+    .then(num => {
+        if (num == 1) {
+            res.send({
+                message: "Brewery was deleted successfully!"
+            });
+        } else {
+            res.send({
+                message: `Cannot delete Brewery with id=${id}. Maybe Brewery was not found!`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Could not delete Brewery with id=" + id
+        });
+    });
 };
--- routes/brewery.routes.js
+++ routes/brewery.routes.js
@@ -3,8 +3,15 @@ module.exports = app => {
     const breweries = require("../controllers/brewery.controller.js");
     const router = require("express").Router();
 
-    router.get("/", breweries.findAll);
+    router.post("/", breweries.create);
+    router.delete("/:id", breweries.delete);
+    router.put("/:id", breweries.update);
     router.get("/:id", breweries.findOne);
+
+
+
+
+    router.get("/", breweries.findAll);
     router.get("/:limit/:offset", breweries.paginate);
 
     app.use('/api/breweries', router);
