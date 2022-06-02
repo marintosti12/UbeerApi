447fc2119eef1fa5c2fca55a037b11fbd8f2b44c
marintosti12 <marin.tostivint@epsi.fr>
2022-06-02 18:03:30 +0200

    rework  cors

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 
-
-app.use(express.json({ extended: false }));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
-
-app.use(cors({
-    origin: '*'
-}));
 
 const db = require("./models");
 
