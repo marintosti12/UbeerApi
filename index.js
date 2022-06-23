b1d8a22837b95c04b94505bede13bf5fecf69a1e
marintosti12 <marin.tostivint@epsi.fr>
2022-06-23 20:00:44 +0200

    fix jwt

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const { auth } = require('express-oauth2-jwt-bearer');
 const app = require('./app');
 const db = require("./models");
-var jwt = require('express-jwt');
-const jwks = require('jwks-rsa');
 
-const jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: 'https://dev-5e00y2tf.us.auth0.com/.well-known/jwks.json'
-    }),
+
+const checkJwt = auth({
     audience: 'https://ubeer-app.herokuapp.com/',
-    issuer: 'https://dev-5e00y2tf.us.auth0.com/',
-    algorithms: ['RS256']
+    issuerBaseURL: `https://dev-5e00y2tf.us.auth0.com/`,
 });
 
-app.use(jwtCheck);
 db.sequelize.sync({ force: true }).then(() => {
     console.log("Drop and re-sync db.");
 });
